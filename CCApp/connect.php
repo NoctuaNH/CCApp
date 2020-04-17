7581de93f23d87740f0a96423f14d8e91ac98049
NoctuaNH <63804923+NoctuaNH@users.noreply.github.com>
2020-04-17 18:09:54 +0100

    Update connect.php

--- CCApp/connect.php
+++ CCApp/connect.php
@@ -1,9 +1,14 @@
 <?php
 //Connection to Returns database
 function db(){
-	global $link;
-	$link = mysqli_connect("localhost", "root", "", "returnsdb", "3308")
-	or die("Cannot establish connection to database");
-	return $link;
-	}
+    define("HOST", "returnsdb.ch3riiwjvdch.us-east-1.rds.amazonaws.com");
+    define ("DBUSER", "dbadmin");
+    define ("PASS", "db_password");
+    define ("DB", "returnsdb");
+    define ("PORT", "3306");
+    $link = mysqli_connect(HOST, DBUSER, PASS, DB, PORT)
+    or die("Cannot establish connection to database");
+     $database = mysqli_select_db(DB) or die("Cannot establish connection to databse");
+    return $link;
+    }
 ?>
